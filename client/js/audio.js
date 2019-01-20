bd49f10a4dd04870de964f42183005c879ce3983
jen <jennifer.cz.liu@gmail.com>
2019-01-20 00:30:09 -0500

    timer js

--- client/js/audio.js
+++ client/js/audio.js
@@ -39,6 +39,7 @@ function startRecording() {
 
     console.log("recordButton clicked");
     sendPhotos();
+    timer();
 
     /*
         Simple constraints object, for more advanced audio features see
@@ -186,6 +187,34 @@ function createDownloadLink(blob) {
 
     //add the li element to the ol
     recordingsList.appendChild(li);
+}
+
+function timer(){
+    var target_minutes = document.getElementById('time_target').value
+    var countDownDate = new Date().getTime() + target_minutes*1000*60;
+    // var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+        // Update the count down every 1 second
+    var x = setInterval(function() {
+
+      // Get todays date and time
+      var now = new Date().getTime();
+
+      // Find the distance between now and the count down date
+      var distance = countDownDate - now;
+
+      // Time calculations for days, hours, minutes and seconds
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      // Display the result in the element with id="timer"
+      document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+
+      // If the count down is finished, write some text
+      if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Target time reached";
+      }
+    }, 1000);
 }
 
 function sendAudio(blob) {
